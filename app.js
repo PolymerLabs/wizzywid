f098cbe35212c8435b5b8d5a37287a44ec418f7d
Monica Dinculescu <noms@google.com>
2017-07-18 15:09:32 -0700

    refactor: move actionHistory inside of app-shell

--- app.js
+++ app.js
@@ -26,13 +26,6 @@ window.addEventListener('WebComponentsReady', function() {
   }, true);
 
   Polymer.Gestures.addListener(viewContainer, 'track', trackElement);
-
-  document.addEventListener('undo', function() {
-    actionHistory.undo();
-  });
-  document.addEventListener('redo', function() {
-    actionHistory.redo();
-  });
 });
 
 function addNewElement(event) {
@@ -67,7 +60,7 @@ function addNewElement(event) {
   requestAnimationFrame(function() {
     el.click();
   });
-  actionHistory.add('new', el);
+  shell.$.actionHistory.add('new', el);
 }
 
 function addNewSample(event) {
@@ -88,7 +81,7 @@ function addNewSample(event) {
   // Give it a unique ID.
   var newId = makeUniqueId(el, tag);
   el.id = newId;
-  actionHistory.add('new', el);
+  shell.$.actionHistory.add('new', el);
   maybeDoDefaultProperties(el.tagName.toLowerCase(), el);
 
   // You need the item to render first.
@@ -130,14 +123,14 @@ function deleteElement(event) {
 
   // Deleting the whole app should remove the children I guess.
   if (el.id === 'viewContainer') {
-    actionHistory.add('delete', el, {innerHtml: el.innerHTML});
+    shell.$.actionHistory.add('delete', el, {innerHtml: el.innerHTML});
     el.innerHTML = '';
     updateActiveElement(el);
   } else {
     var parent = el.parentElement
     parent.removeChild(el);
     updateActiveElement(parent);
-    actionHistory.add('delete', el, {parent: parent});
+    shell.$.actionHistory.add('delete', el, {parent: parent});
   }
 }
 
@@ -146,7 +139,7 @@ function fitElement(event) {
   if (!el || el.id === 'viewContainer') {
     return;
   }
-  actionHistory.add('fit', shell.activeElement,
+  shell.$.actionHistory.add('fit', shell.activeElement,
     {oldPosition: el.style.position,
     newPosition: 'absolute',
     oldLeft: el.style.left, oldTop: el.style.top,
@@ -171,7 +164,7 @@ function elementWasUpdated(event) {
   var detail = event.detail;
   var oldValue = shell.updateActiveElementValues(detail.type, detail.name, detail.value);
   treeView.recomputeTree(viewContainer, shell.activeElement);
-  actionHistory.add('update', shell.activeElement,
+  shell.$.actionHistory.add('update', shell.activeElement,
       {type: detail.type, name: detail.name, newValue: detail.value, oldValue: oldValue});
 }
 
@@ -270,12 +263,12 @@ function dragElement(event, el) {
         }
         window._dropTarget.appendChild(el);
         window._dropTarget.classList.remove('over');
-        actionHistory.add('reparent', el, {newParent: window._dropTarget, oldParent: oldParent});
+        shell.$.actionHistory.add('reparent', el, {newParent: window._dropTarget, oldParent: oldParent});
         window._dropTarget = null;
       } else if (el.parentElement && (el.parentElement !== viewContainer)) {
         // If there's no drop target and the el used to be in a different
         // parent, move it to the main view.
-        actionHistory.add('reparent', el, {newParent: viewContainer, oldParent: el.parentElement});
+        shell.$.actionHistory.add('reparent', el, {newParent: viewContainer, oldParent: el.parentElement});
         el.parentElement.removeChild(el);
         viewContainer.appendChild(el);
       }
@@ -293,7 +286,7 @@ function dragElement(event, el) {
         el.style.top = local.top - parent.top + 'px';
       }
 
-      actionHistory.add('move', el,
+      shell.$.actionHistory.add('move', el,
           {newLeft: el.style.left, newTop: el.style.top, oldLeft: oldLeft, oldTop: oldTop,
           oldPosition:oldPosition, newPosition: el.style.position});
       el.classList.remove('dragging');
@@ -316,7 +309,7 @@ function resizeElement(event, el) {
       break;
     case 'end':
       window._resizing = false;
-      actionHistory.add('resize', el,
+      shell.$.actionHistory.add('resize', el,
           {newWidth: el.style.width, newHeight: el.style.height,
            oldWidth: window._initialWidth + 'px', oldHeight: window._initialHeight + 'px'});
       el.classList.remove('resizing');
@@ -337,7 +330,7 @@ function moveElementUp(event) {
   if (el.id === 'viewContainer' || parent.id === 'viewContainer') {
     return;
   }
-  actionHistory.add('move-up', el, {oldParent: parent, newParent: parent.parentElement});
+  shell.$.actionHistory.add('move-up', el, {oldParent: parent, newParent: parent.parentElement});
   parent.removeChild(el);
   parent.parentElement.appendChild(el);
   displayElement();
@@ -356,7 +349,7 @@ function moveElementBack(el, updateHistory) {
     parent.appendChild(el);
   }
   if (updateHistory) {
-    actionHistory.add('move-back', el);
+    shell.$.actionHistory.add('move-back', el);
   }
   displayElement();
 }
@@ -380,7 +373,7 @@ function moveElementForward(el, updateHistory) {
     parent.insertBefore(el, parent.firstChild);
   }
   if (updateHistory) {
-    actionHistory.add('move-forward', el);
+    shell.$.actionHistory.add('move-forward', el);
   }
   displayElement();
 }
